65a292ade6528c0c609fc08dd16061586f7d4dc6
yunlonggao-mega <yunlong@megaeth.technoloy>
2025-05-10 09:37:31 +0800

    fix ws port

--- simulators/optimism/rpc/helper.go
+++ simulators/optimism/rpc/helper.go
@@ -66,7 +66,7 @@ func runHTTP(t *hivesim.T, l2 *optimism.L2Node, v *vault, fn func(*TestEnv)) {
 // runWS runs the given test function using the WebSocket RPC client.
 func runWS(t *hivesim.T, l2 *optimism.L2Node, v *vault, fn func(*TestEnv)) {
 	ctx, done := context.WithTimeout(context.Background(), 5*time.Second)
-	rpcClient, err := rpc.DialWebsocket(ctx, fmt.Sprintf("ws://%v:%d/", l2.Client.IP, l2.HTTPPort), "")
+	rpcClient, err := rpc.DialWebsocket(ctx, fmt.Sprintf("ws://%v:%d/", l2.Client.IP, l2.WSPort), "")
 	done()
 	if err != nil {
 		t.Fatal("WebSocket connection failed:", err)
